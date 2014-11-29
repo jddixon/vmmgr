f05a2e6f2c83d9fcfdd29ae408112a5479c63790
Jim Dixon <jddixon@gmail.com>
2014-11-28 17:34:58 -0800

    proj: rudimentary testSetup.py succeeds

--- testSetup.py
+++ testSetup.py
@@ -3,7 +3,7 @@
 # testSetup.py
 
 import boto, os,  unittest
-import boto.vpc
+import boto.ec2, boto.vpc
 
 from vmmgr import *
 
@@ -16,16 +16,28 @@ class TestSetup (unittest.TestCase):
 
     def testVmMgrSetup(self):
         for rNdx, region in enumerate(REGIONS):
-            vpcCnx = boto.vpc.VPCConnection(region)
-            print "%-9s %s" % (region, VPC_CIDRS[rNdx])
-            # igw = vpcCnx.create_internet_gateway()
+            regionInfo = boto.ec2.get_region(region)
+            self.assertTrue( regionInfo is not None)
+
+            # This was yielding a very misleading AuthFailure further
+            # down the line because a string like 'eu-west-1' was supplied
+            # instead of the corresponding EC2RegionInfo object
+            vpcCnx = boto.vpc.VPCConnection(region=regionInfo)
+            self.assertTrue(vpcCnx is not None)
+
+            print "%-9s %s" % (regionInfo, VPC_CIDRS[rNdx])
+            
             print "  gateway: %s" % GATEWAY_IDS[rNdx]
+            igws = vpcCnx.get_all_internet_gateways()   # AuthFailure !
+            self.assertEqual(len(igws), 1)
+            self.assertEqual(str(igws[0]), 'InternetGateway:' +GATEWAY_IDS[rNdx])
+
             for zNdx, zone in enumerate(ZONES[rNdx]):
                 print "    zone %s, cidr %s" % (zone, SUBNET_CIDRS[rNdx][zNdx])
-                sub = vpcCnx.create_subnet(VPC_IDS[rNdx],
-                        SUBNET_CIDRS[rNdx][zNdx],
-                        availability_zone=zone)
-                print "SUBNET: %z" % sub
+                #sub = vpcCnx.create_subnet(VPC_IDS[rNdx],
+                #        SUBNET_CIDRS[rNdx][zNdx],
+                #        availability_zone=zone)
+                #print "SUBNET: %z" % sub
 
 if __name__ == '__main__':
     unittest.main()
